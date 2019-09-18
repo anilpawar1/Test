95ad9b05e38d696cff3f94ff7acbf642cecd813c
Anil Pawar <mr.anilpawar@gmail.com>
2019-09-18 11:47:00 +0530

    Update Swagger2Config.java

--- src/main/java/test/Swagger2Config.java
+++ src/main/java/test/Swagger2Config.java
@@ -1,5 +1,5 @@
 package test;
-
+/**
 import static springfox.documentation.builders.PathSelectors.regex;
 import java.util.Collections;
 import org.springframework.context.annotation.Bean;
@@ -12,12 +12,12 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-@Configuration
-@EnableSwagger2
+**/
+//@Configuration
+//@EnableSwagger2
 public class Swagger2Config {
-    @Bean
-    public Docket api() {
+    //@Bean
+   /** public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2).select()
             .apis(RequestHandlerSelectors
                 .basePackage("endpoints.impl"))
@@ -33,4 +33,4 @@ public class Swagger2Config {
             .version("1.0.0")
             .build();
     }
-}
+**/}
