0af7fc17dac7223e60b43367a5b79d8cf545af8d
Anil Pawar <mr.anilpawar@gmail.com>
2019-10-17 18:11:38 +0530

    Update StringUtils.java

--- src/main/java/util/StringUtils.java
+++ src/main/java/util/StringUtils.java
@@ -3,7 +3,7 @@ package util;
 
 public final class StringUtils {
 	private StringUtils() {}
-	
+	// test comment added for jenkins test.
 	public static boolean isValidString(String string) {
 		return string != null && !string.isEmpty();
 	}
